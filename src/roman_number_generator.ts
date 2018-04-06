a80015a4edd6d6cfae7c2e2bfd28ee57170a723a
Michael Smith <mike@spokefire.co.uk>
2018-04-06 18:04:14 +0100

    Make M, C, L and X conversions work

--- src/roman_number_generator.ts
+++ src/roman_number_generator.ts
@@ -1,7 +1,7 @@
 class RomanNumberGenerator {
   public static generate(num: number): string {
-    const humanNumbers: number[] = [5, 1];
-    const numerals: string[] = ["V", "I"];
+    const humanNumbers: number[] = [1000, 100, 50, 10, 5, 1];
+    const numerals: string[] = ["M", "C", "L", "X", "V", "I"];
     let convertedString: string = "";
 
     for (let i = 0; i < humanNumbers.length; i++) {
