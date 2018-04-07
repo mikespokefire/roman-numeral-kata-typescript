e068f07ddf47705a3a12ad78e7fc917c6f0e0d1f
Michael Smith <mike@spokefire.co.uk>
2018-04-07 13:46:09 +0100

    Refactor to be more Object Orientated
    Since we now have a working version of the code, lets split out all the
    parts of it to make it easier to work with in future by giving the
    various parts of it meaning.

--- src/roman_number_generator.ts
+++ src/roman_number_generator.ts
@@ -1,21 +1,40 @@
 class RomanNumberGenerator {
   public static generate(num: number): string {
-    if (num <= 0 || num >= 4000) {
+    return new RomanNumberGenerator(num).generate();
+  }
+
+  private num: number;
+
+  private humanNumbers: number[] = [
+    1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1,
+  ];
+  private numerals: string[] = [
+    "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I",
+  ];
+
+  constructor(num: number) {
+    this.num = num;
+  }
+
+  public generate(): string {
+    if (!this.isValid()) {
       throw new Error("Number must be greater than 0 and less than 4000");
     }
 
-    const humanNumbers: number[] = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    const numerals: string[] = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     let convertedString: string = "";
 
-    for (let i = 0; i < humanNumbers.length; i++) {
-      while (num >= humanNumbers[i]) {
-        convertedString += numerals[i];
-        num -= humanNumbers[i];
+    for (let i = 0; i < this.humanNumbers.length; i++) {
+      while (this.num >= this.humanNumbers[i]) {
+        convertedString += this.numerals[i];
+        this.num -= this.humanNumbers[i];
       }
     }
 
     return convertedString;
+  }
+
+  private isValid(): boolean {
+    return (this.num > 0 && this.num < 4000);
   }
 }
 
